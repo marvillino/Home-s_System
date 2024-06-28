c0815607ad21aa42bb5ce2d6aa344a9632befe81
Marvin Wahid <marvin.wahed.fouad@gmail.com>
2024-06-28 09:14:57 +0300

    FrontEnd done

---
+++ src/FrontEnd/HomeS_System.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package FrontEnd;
+
+/**
+ *
+ * @author Blu-Ray
+ */
+public class HomeS_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Family ages");
+        System.out.println("family work");
+        System.out.println("family gender");
+        System.out.println("family role");
+        
+    }
+    
+}
